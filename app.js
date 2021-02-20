2c567247d263c4d3de20c046e25a6e3573311dfe
AbzG97 <abdulla.ghazalah@hotmail.com>
2021-02-20 12:53:44 -0500

    rewrote utils.js code to work with promises and switched API to openweathermap

--- app.js
+++ app.js
@@ -1,31 +1,12 @@
-
 const weather_app_utils = require("./utils.js");
-
-
 const address = process.argv[2];
 
 if(!address){
     console.log("please provide an address");
 } else {
-    // getting location and weather for the location using callback chaining
-    weather_app_utils.GetLocation(address, (error, location_data) => {
-        if(error){
-            return console.log("Error", error);
-        }
-        weather_app_utils.forecast(location_data.latitude, location_data.longtitude, (error, {temperature, weather_descriptions, precip_chance} = {}) => {
-            if(error){
-                return console.log('Error', error);
-            }
-
-            console.log(`Location data\n latitude: ${location_data.latitude},  longtitude: ${location_data.longtitude}, location: ${location_data.location}`);
-            console.log(`Today's forecast\n tempreature: ${temperature}, it's ${weather_descriptions}, with %${precip_chance} chance of rain.`);
-        
-
-        });
-        
-    });
+    weather_app_utils(address);
 
 }
 
-console.log("this just got added");
+
 
--- utils.js
+++ utils.js
@@ -1,48 +1,40 @@
+const request_promise = require("request-promise");
 
-const request = require("request");
-
-const GetLocation = (address, callback) => {
+const GetWeather = (address) => {
     // getting the location
-    const geocodeURL = `http://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoidmFuaWxsYXVuaWNvcm4iLCJhIjoiY2toeXk5aHpuMGk4ZDJ5cXE2YWZhczQwMyJ9.TEEfNWliK7gh6uVONOA0WQ&limit=1`;
-
-    request({url: geocodeURL, json: true}, (error, response) => {
-        if(error){
-            callback("error unable to connect to the service", undefined);
-        }else if(response.body.features.length == 0){
-            callback("unable to find location", undefined);
-
-        } else {
-            callback(undefined, {
-                latitude: response.body.features[0].geometry.coordinates[1],
-                longtitude: response.body.features[0].geometry.coordinates[0],
-                location: response.body.features[0].place_name
-            });
-
+    const geocodeURL = `http://api.openweathermap.org/geo/1.0/direct?q=${address}&limit=1&appid=c56f6f5adad7d73cd791504a079198d8`;
+    request_promise(geocodeURL).then((data) => {
+       // console.log(parsed_data);
+        //console.log("data returned");
+        const parsed_data = JSON.parse(data);
+        //console.log(jsonData);
+        const location_data =  {
+            name: parsed_data[0].name,
+            country: parsed_data[0].country,
+            lat: parsed_data[0].lat,
+            lon: parsed_data[0].lon
         }
+        request_promise(`http://api.openweathermap.org/data/2.5/weather?lat=${location_data.lat}&lon=${location_data.lon}&units=metric&appid=c56f6f5adad7d73cd791504a079198d8`)
+        .then((weather_data) => {
+            const parsed_weather_data = JSON.parse(weather_data);
+            const forecast = {
+                place_name: parsed_weather_data.name,
+                weather_desc: parsed_weather_data.weather[0].description,
+                temp: parsed_weather_data.main.temp,
+                feels_like: parsed_weather_data.main.feels_like,
+                humidity: parsed_weather_data.main.humidity,
+            }
+            console.log(forecast);
+        }).catch((e) => {
+            console.log("unable to connect to forecast services");
+            console.log(e);
+        })
+    }).catch ((e) => {
+        console.log("unable to connect to location services");
+        console.log(e);
     });
 }
 
+//GetWeather("Mississauga, Ontario");
 
-  // getting the weather
-  const forecast = (latitude, longtitude, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=cec6f01743fcd7a06557c4716e545bc7&query=${encodeURIComponent(latitude)},${encodeURIComponent(longtitude)}`;
-    request({url: url, json: true}, (error, response) => {
-        if(error){
-            callback(undefined, "unable to connect to weather service");
-        } else if(response.body.error){
-            callback(undefined, "unable to find location");
-        } else {
-            callback(undefined, {
-                temperature: response.body.current.temperature,
-                weather_descriptions: response.body.current.weather_descriptions,
-                precip_chance: response.body.current.precip
-            });
-
-        }
-    });
-}
-
-module.exports = {
-    GetLocation: GetLocation,
-    forecast: forecast
-};
+module.exports = GetWeather;
